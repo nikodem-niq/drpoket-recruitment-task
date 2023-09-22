84f9e65c53b0b45bde68359c266dc1d593c6c13e
Nikodem <nikodem.zychma@gmail.com>
2023-09-22 14:32:31 +0200

    feat: task-two update

--- task-two/frontend/src/Pages/MyPath/index.tsx
+++ task-two/frontend/src/Pages/MyPath/index.tsx
@@ -1,9 +1,29 @@
+import { useQuery } from "react-query";
 import Map from "../../components/Map";
 
-function MyPath() {
+const fetchPaths = async () => {
+  const response = await fetch('http://localhost:3002/path/', {
+    headers: {
+      method: 'GET'
+    }
+  });
+  const data = await response.json();
+  return data.paths[2];  // mocked that 1st element is the path we are looking for
+};
+
+const MyPath = () => {
+  const { data: userPath, isLoading, isError } = useQuery('userPath', fetchPaths);
+  if(isLoading) {
+    return <div className='w-80vw flex items-center justify-center py-10 text-3xl'>Loading data...</div>
+  }
+
+  if(isError) {
+    return <div className='w-80vw flex items-center justify-center py-10 text-3xl'>Error, try to refresh!</div>
+  }
+
   return (
   <div className="w-80vw flex items-center justify-center py-10 text-3xl">
-      <Map/>
+      <Map path={userPath.points}/>
   </div>
   );
 }
--- task-two/frontend/src/components/Map/index.tsx
+++ task-two/frontend/src/components/Map/index.tsx
@@ -1,25 +1,59 @@
-import GoogleMapReact from 'google-map-react';
+import { GoogleMap, LoadScript, Polyline } from '@react-google-maps/api';
+import { PathFromPoints } from '../../types/PathFromPoints';
+import { useState, useEffect } from 'react';
 
-const Map = () => {
+interface MapProps {
+  path: PathFromPoints
+}
+
+// further implementations: add functionality for handling more than 1 route from db
+
+const Map: React.FC<MapProps> = ({ path }) => {
+  const DEFAULT_ZOOM = 11;
   const mapsApiKey = import.meta.env.VITE_REACT_GOOGLE_MAPS_API_KEY;
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
+  const [routeCoordinates, setRouteCoordinates] = useState<{lat: number, lng: number}[]>([]);
+  const [isPathShowed, setPathShowed] = useState<boolean>(false);
 
-    return (
-      <div className='w-screen h-96'>
-        {mapsApiKey && 
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: mapsApiKey }}
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}>
-        </GoogleMapReact>}
-      </div>
-    );
+  useEffect(() => {
+    const coordinates = path.map(point => ({
+      lat: point.latitude,
+      lng: point.longitude
+    }));
+    setRouteCoordinates(coordinates);
+  }, [path, isPathShowed]);
+
+  const handlePathShowing = () => {
+    setPathShowed(true);
   }
+
+  return (
+    <div className='flex flex-col'>
+      <div className='w-screen h-96 mb-12'>
+        {mapsApiKey && routeCoordinates && 
+          <LoadScript googleMapsApiKey={mapsApiKey}>
+            <GoogleMap
+              mapContainerStyle={{ width: '100%', height: '100%' }}
+              center={routeCoordinates[0]}
+              zoom={DEFAULT_ZOOM}
+            >
+              {
+                isPathShowed &&
+                <Polyline 
+                path={routeCoordinates}
+                options={{
+                  strokeColor: "#1e4aa8",
+                  strokeOpacity: 0.7,
+                  strokeWeight: 4
+                }}
+                />
+              }
+            </GoogleMap>
+          </LoadScript>
+        }
+      </div>
+      <button onClick={() => {handlePathShowing()}} disabled={isPathShowed} className='bg-blue-500 disabled:opacity-50 disabled:bg-slate-700 hover:bg-blue-700 text-white font-bold text-2xl py-2 px-4 rounded w-1/1'>Pokaż przejechaną trasę</button>
+    </div>
+  );
+}
 
 export default Map;
